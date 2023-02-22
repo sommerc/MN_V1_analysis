ccb4b138a7f83781b2dcc895f8f23b1b00eb499b
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-02-22 13:23:34 +0100

    added tail freq analysis

---
+++ frequency_analysis.py
@@ -0,0 +1,181 @@
+import os
+import pathlib
+from collections import defaultdict
+
+import cv2
+import h5py
+import yaml
+import numpy as np
+import pandas as pd
+import roifile
+import seaborn as sns
+import tadpose
+from matplotlib import pyplot as plt
+from matplotlib import colors as mpl_colors
+from skimage import draw
+from tqdm.auto import tqdm, trange
+from scipy.ndimage import gaussian_filter1d
+
+import pywt
+from scipy import signal
+
+from shared import settings, get_good_tracks
+
+
+def compute_angle_wavlet_psd_mean(tad, tid, scales, wavelet, a, b, c, cfgs):
+    ang = tadpose.analysis.angles(tad, (a, b), (b, c), win=None, track_idx=tid)
+    ang_smooth = gaussian_filter1d(ang, cfgs["FREQ_TEMP_ANGLE_SMOOTH"])
+    ang_zscore = (ang_smooth - ang_smooth.mean()) / ang_smooth.std()
+
+    t_wvlt, freq = pywt.cwt(
+        ang_zscore,
+        scales,
+        wavelet,
+        sampling_period=1 / 60,
+        method="fft",
+    )
+    t_wvlt_psd = np.abs(t_wvlt.T) ** 2
+
+    speed_px_per_frame = tad.speed(
+        cfgs["FREQ_MOVING_NODE"], track_idx=tid, pre_sigma=60, sigma=30
+    )
+    speed_calib = speed_px_per_frame * tadpose.utils.calibrate_by_dish(tad, 14)
+
+    moving_bin = speed_calib > cfgs["FREQ_MOVING_NODE_THRESH"]
+    computed_on = moving_bin.sum() / tad.nframes
+
+    mean_psd_moving = t_wvlt_psd[moving_bin].mean(0)
+
+    return mean_psd_moving, freq, computed_on
+
+
+def dominant_freqency(sig, freq):
+    peaks, props = signal.find_peaks(sig, height=(None, None))
+
+    if len(peaks) > 0:
+
+        peak_pos = np.argmax(props["peak_heights"])
+        return freq[peaks[peak_pos]]
+
+
+def get_frequency_params(cfg):
+    N = cfg["FREQUENCY_N"]
+    wavelet = cfg["FREQUENCY_WAVELET"]
+
+    # Fc = pywt.central_frequency(wavelet)
+    fps = 60
+    sp = 1 / fps
+
+    scales = 2 ** np.linspace(1, 6, N)  # <- dyadic 1-30HZ
+    frequencies = pywt.scale2frequency(wavelet, scales) / sp
+    return scales, frequencies, wavelet
+
+
+def frequency_analysis(all_movs, stg, nodes, cfg):
+    cfgs = cfg[stg]
+    scales, freq, wavelet = get_frequency_params(cfg)
+    tab_all = []
+    for fn in tqdm(all_movs):
+        gen = fn.parent.stem
+        stg = fn.parent.parent.stem
+
+        tail_a, tail_b, tail_c = nodes
+
+        tad = tadpose.Tadpole.from_sleap(str(fn))
+        track_okay_idx = get_good_tracks(tad.analysis_file, cfgs, tail_b)
+
+        file_path = tad.video_fn
+        base_file = os.path.basename(file_path)[:-4]
+
+        aligner = tadpose.alignment.RotationalAligner(
+            central_part=cfgs["ALIGN_CENTRAL"], aligned_part=cfgs["ALIGN_TOP"]
+        )
+
+        aligner.tracks_to_align = track_okay_idx
+
+        tad.aligner = aligner
+
+        for tid in track_okay_idx:
+            mean_psd_moving, freq, computed_on = compute_angle_wavlet_psd_mean(
+                tad, tid, scales, wavelet, tail_a, tail_b, tail_c, cfgs
+            )
+
+            dom_freq = dominant_freqency(mean_psd_moving, freq)
+
+            tab_all.append(
+                [
+                    base_file,
+                    stg,
+                    gen,
+                    tid,
+                    dom_freq,
+                    computed_on,
+                ]
+                + mean_psd_moving.tolist()
+            )
+
+    tab_freq = pd.DataFrame(
+        tab_all,
+        columns=[
+            "Movie",
+            "Stage",
+            "Genotype",
+            "Track_idx",
+            "dominant_freq",
+            "computed_on_ratio",
+        ]
+        + freq.tolist(),
+    )
+
+    return tab_freq
+
+
+def run_stage(STAGE, cfg):
+    cfgs = cfg[STAGE]
+    ROOT_DIR = pathlib.Path(cfgs["ROOT_DIR"])
+
+    all_movs = list(ROOT_DIR.rglob("*.mp4"))
+    print(f"Processing Stage {STAGE} with {len(all_movs)} movies")
+
+    tab_freq_dict = {}
+    if "FREQ_FOR" in cfgs:
+
+        for name, nodes in cfgs["FREQ_FOR"].items():
+            tab_freq = frequency_analysis(all_movs, STAGE, nodes, cfg)
+
+            tab_freq.to_csv(
+                f"{cfg['FREQUENCY_OUTDIR']}/frequency_{STAGE}_{name}_res.tab", sep="\t"
+            )
+            tab_freq_dict[name] = tab_freq
+    else:
+        print("  -- no FREQ_FOR set")
+
+    return tab_freq_dict
+
+
+def run(STAGES, cfg):
+    tab_dict_all = []
+
+    for STAGE in STAGES:
+        tab_dict_stg = run_stage(STAGE, cfg)
+        tab_dict_all.append(tab_dict_stg)
+
+    # tab_all = pd.concat(tab_all, axis=0, ignore_index=True)
+    # tab_all.to_csv(f"{cfg['FREQUENCY_OUTDIR']}/frequency_res.tab", sep="\t")
+    # return tab_stg
+
+
+if __name__ == "__main__":
+    cfg = settings()
+    os.makedirs(cfg["FREQUENCY_OUTDIR"], exist_ok=True)
+
+    STAGES = [
+        # "37-38",
+        # "44-48",
+        "52-54",
+        "57-58",
+        "59-62",
+        "63-64",
+        "Juv",
+    ]
+    run(STAGES, cfg)
---
+++ frequency_plots.py
@@ -0,0 +1,76 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib import colors as mpl_colors
+from tqdm.auto import tqdm, trange
+
+from shared import settings
+
+
+if __name__ == "__main__":
+    cfg = settings()
+    STAGES = [
+        "37-38",
+        "44-48",
+        "52-54",
+        "57-58",
+        "59-62",
+        "63-64",
+        "Juv",
+    ]
+    OUT_DIR = cfg["FREQUENCY_OUTDIR"]
+
+    for STAGE in STAGES:
+        cfgs = cfg[STAGE]
+        if "FREQ_FOR" in cfgs:
+            for name, nodes in cfgs["FREQ_FOR"].items():
+                tab_stg_freq = pd.read_csv(
+                    f"{cfg['FREQUENCY_OUTDIR']}/frequency_{STAGE}_{name}_res.tab",
+                    sep="\t",
+                    index_col=0,
+                )
+
+                print(tab_stg_freq)
+
+                _, tail_ptn, _ = nodes
+
+                f, ax = plt.subplots(
+                    1, tab_stg_freq.Genotype.nunique(), sharey=True, figsize=(20, 4)
+                )
+                cc = ax[0]._get_lines.prop_cycler
+                for i, (gen, tab) in enumerate(tab_stg_freq.groupby("Genotype")):
+                    a = tab.T.iloc[6:].mean(1)
+                    b = tab.T.iloc[6:].std(1)
+                    x_vals = a.index.astype("float")
+                    color = next(cc)["color"]
+                    ax[i].plot(x_vals, a, color=color)
+                    ax[i].fill_between(x_vals, (a - b), (a + b), alpha=0.3, color=color)
+                    ax[i].set_title(gen)
+                    ax[i].set_ylim(0, 10)
+                    sns.despine(ax=ax[i])
+                    ax[i].set_xlabel(f"Frequency of angle change at {tail_ptn}")
+                    ax[i].set_ylabel("Mean CWT power spectrum")
+                plt.tight_layout()
+
+                plt.savefig(f"{OUT_DIR}/cwt_mean_ps_{STAGE}_{name}_{tail_ptn}.pdf")
+
+                feature = "dominant_freq"
+                f, ax = plt.subplots(figsize=(14, 4))
+
+                b = sns.stripplot(
+                    y=feature,
+                    x="Genotype",
+                    data=tab_stg_freq,
+                    ax=ax,
+                    hue="Genotype",
+                    dodge=False,
+                    zorder=1,
+                    legend=False,
+                )
+                sns.despine(ax=ax)
+                ax.set_title(f"{STAGE}_{name}_{tail_ptn}")
+                plt.savefig(
+                    f"{OUT_DIR}/dominant_cwt_freq_{STAGE}_{name}_{tail_ptn}.pdf"
+                )
