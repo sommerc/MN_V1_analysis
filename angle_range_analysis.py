f814472144f5084f1d4d371982eea26c2b59bad0
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-02-23 13:22:38 +0100

    new angle range analysis/plots

---
+++ angle_range_analysis.py
@@ -0,0 +1,168 @@
+import os
+import pathlib
+from collections import defaultdict
+
+import cv2
+import h5py
+import yaml
+import numpy as np
+import pandas as pd
+import roifile
+import seaborn as sns
+import tadpose
+from matplotlib import pyplot as plt
+from matplotlib import colors as mpl_colors
+from skimage import draw
+from tqdm.auto import tqdm, trange
+from scipy.ndimage import gaussian_filter1d
+from shared import settings, get_good_tracks
+
+
+def angle_range(all_movs, stg, nodes, cfg):
+    cfgs = cfg[stg]
+
+    tab_all = []
+    for fn in tqdm(all_movs):
+        gen = fn.parent.stem
+        stg = fn.parent.parent.stem
+
+        tail_a, tail_b, tail_c = nodes
+
+        tad = tadpose.Tadpole.from_sleap(str(fn))
+        track_okay_idx = get_good_tracks(tad.analysis_file, cfgs, tail_b)
+
+        file_path = tad.video_fn
+        base_file = os.path.basename(file_path)[:-4]
+
+        aligner = tadpose.alignment.RotationalAligner(
+            central_part=cfgs["ALIGN_CENTRAL"], aligned_part=cfgs["ALIGN_TOP"]
+        )
+
+        aligner.tracks_to_align = track_okay_idx
+
+        tad.aligner = aligner
+
+        for tid in track_okay_idx:
+            ang = tadpose.analysis.angles(
+                tad, (tail_a, tail_b), (tail_b, tail_c), win=None, track_idx=tid
+            )
+            ang_smooth = gaussian_filter1d(ang, cfgs["FREQ_TEMP_ANGLE_SMOOTH"])
+            # ang_p95_5_dist = np.percentile(ang_smooth, 95) - np.percentile(
+            #     ang_smooth, 5
+            # )
+
+            speed_px_per_frame = tad.speed(
+                cfgs["FREQ_MOVING_NODE"],
+                track_idx=tid,
+                pre_sigma=cfgs["LOCOMOTION_SPATIAL_SIGMA"],
+                sigma=cfgs["LOCOMOTION_SPATIAL_SIGMA"],
+            )
+            speed_calib = speed_px_per_frame * tadpose.utils.calibrate_by_dish(tad, 14)
+
+            moving_bin = speed_calib > cfgs["FREQ_MOVING_NODE_THRESH"]
+
+            if moving_bin.sum() > 0:
+                ang_std_mov = ang_smooth[moving_bin].std()
+            else:
+                ang_std_mov = 0
+
+            tab_all.append([base_file, stg, gen, tid, ang_smooth.std(), ang_std_mov])
+
+        tab_ar = pd.DataFrame(
+            tab_all,
+            columns=[
+                "Movie",
+                "Stage",
+                "Genotype",
+                "Track_idx",
+                "angle_std",
+                "angle_moving_std",
+            ],
+        )
+    return tab_ar
+
+
+def run_stage(STAGE, cfg):
+    cfgs = cfg[STAGE]
+    ROOT_DIR = pathlib.Path(cfgs["ROOT_DIR"])
+
+    all_movs = list(ROOT_DIR.rglob("*.mp4"))
+    print(f"Processing Stage {STAGE} with {len(all_movs)} movies")
+
+    tab_ar_dict = {}
+    if "ANGLE_RANGE_FOR" in cfgs:
+
+        for name, nodes in cfgs["ANGLE_RANGE_FOR"].items():
+            tab_ar = angle_range(all_movs, STAGE, nodes, cfg)
+
+            tab_ar.to_csv(
+                f"{cfg['ANGLE_RANGE_OUTDIR']}/angle_range_{STAGE}_{name}_res.tab",
+                sep="\t",
+            )
+            tab_ar_dict[name] = tab_ar
+    else:
+        print("  -- no ANGLE_RANGE_FOR set")
+
+    return tab_ar_dict
+
+
+def run(STAGES, cfg):
+    tab_all = []
+
+    for STAGE in STAGES:
+        tab_ar_dict = run_stage(STAGE, cfg)
+        tab_all.append(tab_ar_dict)
+
+    tab_collect = []
+    for tab_dict in tab_all:
+        for name, tab in tab_dict.items():
+            tab["angle_at"] = name
+            tab_collect.append(tab)
+
+    tab_collect = pd.concat(tab_collect, axis=0, ignore_index=True)
+    tab_collect.to_csv(f"{cfg['ANGLE_RANGE_OUTDIR']}/angle_range_res.tab", sep="\t")
+    return tab_collect
+
+
+def merge_results():
+    cfg = settings()
+    files = [
+        "angle_range_52-54_tail_stem_res.tab",
+        "angle_range_57-58_leg_ankle_res.tab",
+        "angle_range_57-58_tail_stem_res.tab",
+        "angle_range_59-62_leg_ankle_res.tab",
+        "angle_range_59-62_tail_stem_res.tab",
+        "angle_range_63-64_leg_ankle_res.tab",
+        "angle_range_63-64_tail_stem_res.tab",
+        "angle_range_44-48_tail_stem_res.tab",
+    ]
+
+    TAB = []
+    for fn in files:
+        tab = pd.read_csv(cfg["ANGLE_RANGE_OUTDIR"] + "/" + fn, sep="\t", index_col=0)
+        name = "_".join(fn.split("_")[3:5])
+        tab["angle_at"] = name
+        TAB.append(tab)
+
+    TAB = pd.concat(TAB, axis=0, ignore_index=True)
+    TAB.to_csv(cfg["ANGLE_RANGE_OUTDIR"] + "/" + "angle_range_merged.tab", sep="\t")
+
+
+def main():
+    cfg = settings()
+    os.makedirs(cfg["ANGLE_RANGE_OUTDIR"], exist_ok=True)
+
+    STAGES = [
+        "37-38",
+        "44-48",
+        "52-54",
+        "57-58",
+        "59-62",
+        "63-64",
+        "Juv",
+    ]
+    run(STAGES, cfg)
+
+
+if __name__ == "__main__":
+    main()
---
+++ angle_range_plots.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from shared import settings
+
+
+def plot_by_stage(TAB, cfg):
+    STAGES = [
+        "37-38",
+        "44-48",
+        "52-54",
+        "57-58",
+        "59-62",
+        "63-64",
+        "Juv",
+    ]
+
+    AR_AT = TAB.angle_at.unique()
+
+    OUT_DIR = cfg["ANGLE_RANGE_OUTDIR"]
+
+    for stg in STAGES:
+        for angle_at in AR_AT:
+            tab_sub = TAB[(TAB.Stage == stg) & (TAB.angle_at == angle_at)]
+            if len(tab_sub) > 0:
+                for feature in [
+                    "angle_p95-5_dist",
+                    "angle_std",
+                ]:
+                    f, ax = plt.subplots(figsize=(14, 4))
+                    a = sns.boxenplot(
+                        y=feature,
+                        x="Genotype",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        box_kws={"alpha": 0.4},
+                        dodge=False,
+                    )
+
+                    b = sns.stripplot(
+                        y=feature,
+                        x="Genotype",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        dodge=False,
+                        zorder=1,
+                        legend=False,
+                    )
+                    sns.despine(ax=ax)
+                    ax.set_title(f"{stg} {angle_at}")
+                    plt.savefig(
+                        f"{OUT_DIR}/{stg}_{angle_at}_{feature}.png",
+                        bbox_inches="tight",
+                    )
+                    print(f"{OUT_DIR}/{stg}_{angle_at}_{feature}.png")
+
+
+def plot_by_geno(TAB, cfg):
+    GENO = TAB.Genotype.unique()
+    AR_AT = TAB.angle_at.unique()
+
+    OUT_DIR = cfg["ANGLE_RANGE_OUTDIR"]
+
+    for gen in GENO:
+        for angle_at in AR_AT:
+            tab_sub = TAB[(TAB.Genotype == gen) & (TAB.angle_at == angle_at)]
+            if len(tab_sub) > 0:
+                for feature in [
+                    "angle_p95-5_dist",
+                    "angle_std",
+                ]:
+                    f, ax = plt.subplots(figsize=(14, 4))
+                    a = sns.boxenplot(
+                        y=feature,
+                        x="Stage",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        box_kws={"alpha": 0.4},
+                        dodge=False,
+                    )
+
+                    b = sns.stripplot(
+                        y=feature,
+                        x="Stage",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        dodge=False,
+                        zorder=1,
+                        legend=False,
+                    )
+                    sns.despine(ax=ax)
+                    ax.set_title(f"{gen} {angle_at}")
+                    plt.savefig(
+                        f"{OUT_DIR}/{gen}_{angle_at}_{feature}.png",
+                        bbox_inches="tight",
+                    )
+                    print(f"{OUT_DIR}/{gen}_{angle_at}_{feature}.png")
+
+
+if __name__ == "__main__":
+    TAB = pd.read_csv("angle_range/angle_range_res.tab", sep="\t", index_col=0)
+
+    TAB["angle_std"] = np.rad2deg(TAB["angle_std"])
+    TAB["angle_p95-5_dist"] = np.rad2deg(TAB["angle_p95-5_dist"])
+
+    cfg = settings()
+
+    plot_by_geno(TAB, cfg)
+    plot_by_stage(TAB, cfg)
