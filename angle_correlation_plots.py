3ec22813afdc4ecebaf5687117cd6c64573f27ed
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-03-17 11:31:15 +0100

    plot by name

--- angle_correlation_plots.py
+++ angle_correlation_plots.py
@@ -10,77 +10,87 @@ from shared import settings
 
 
 def plot_by_stage(cfg):
-    OUT_DIR = cfg["AREA_EXPLORED_OUTDIR"]
+    OUT_DIR = cfg["ANGLE_CORR_OUTDIR"]
     TAB = pd.read_csv(f"{OUT_DIR}/angle_correlation_res.tab", sep="\t", index_col=0)
 
     STAGES = TAB.Genotype.unique()
 
-    for stg in STAGES:
-        tab_sub = TAB[(TAB.Stage == stg)]
-        if len(tab_sub) > 0:
-            for feature in ["corr_median", "corr_std", "corr_skewness"]:
-                f, ax = plt.subplots(figsize=(14, 4))
-                a = sns.boxenplot(
-                    y=feature,
-                    x="Genotype",
-                    data=tab_sub,
-                    ax=ax,
-                    hue="Genotype",
-                    box_kws={"alpha": 0.4},
-                    dodge=False,
-                )
-                a.legend_.remove()
-                b = sns.stripplot(
-                    y=feature,
-                    x="Genotype",
-                    data=tab_sub,
-                    ax=ax,
-                    hue="Genotype",
-                    dodge=False,
-                    zorder=1,
-                    legend=False,
-                )
-                sns.despine(ax=ax)
-                ax.set_title(f"{stg} {feature}")
-                plt.savefig(f"{OUT_DIR}/{stg}_{feature}.pdf", bbox_inches="tight")
-                plt.close(f)
+    NAMES = TAB.Name.unique()
+
+    for name in NAMES:
+        for stg in STAGES:
+            tab_sub = TAB[(TAB.Stage == stg) & (TAB.Name == name)]
+
+            if len(tab_sub) > 0:
+                for feature in ["corr_median", "corr_p05", "corr_p95", "corr_skewness"]:
+                    f, ax = plt.subplots(figsize=(14, 4))
+                    a = sns.boxenplot(
+                        y=feature,
+                        x="Genotype",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        box_kws={"alpha": 0.4},
+                        dodge=False,
+                    )
+                    a.legend_.remove()
+                    b = sns.stripplot(
+                        y=feature,
+                        x="Genotype",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Genotype",
+                        dodge=False,
+                        zorder=1,
+                        legend=False,
+                    )
+                    sns.despine(ax=ax)
+                    ax.set_title(f"{stg} {name} {feature}")
+                    plt.savefig(
+                        f"{OUT_DIR}/{name}_{stg}_{feature}.pdf", bbox_inches="tight"
+                    )
+                    plt.close(f)
 
 
 def plot_by_geno(cfg):
-    OUT_DIR = cfg["AREA_EXPLORED_OUTDIR"]
+    OUT_DIR = cfg["ANGLE_CORR_OUTDIR"]
     TAB = pd.read_csv(f"{OUT_DIR}/angle_correlation_res.tab", sep="\t", index_col=0)
 
     GENO = TAB.Genotype.unique()
+    NAMES = TAB.Name.unique()
 
-    for gen in GENO:
-        tab_sub = TAB[(TAB.Genotype == gen)]
-        if len(tab_sub) > 0:
-            for feature in ["corr_median", "corr_std", "corr_skewness"]:
-                f, ax = plt.subplots(figsize=(14, 4))
-                a = sns.boxenplot(
-                    y=feature,
-                    x="Stage",
-                    data=tab_sub,
-                    ax=ax,
-                    hue="Stage",
-                    box_kws={"alpha": 0.4},
-                    dodge=False,
-                )
-                a.legend_.remove()
-                b = sns.stripplot(
-                    y=feature,
-                    x="Stage",
-                    data=tab_sub,
-                    ax=ax,
-                    hue="Stage",
-                    dodge=False,
-                    zorder=1,
-                    legend=False,
-                )
-                sns.despine(ax=ax)
-                ax.set_title(f"{gen} {feature}")
-                plt.savefig(f"{OUT_DIR}/{gen}_{feature}.pdf", bbox_inches="tight")
-                plt.close(f)
+    for name in NAMES:
+        for gen in GENO:
+            tab_sub = TAB[(TAB.Genotype == gen) & (TAB.Name == name)]
+            if len(tab_sub) > 0:
+                for feature in ["corr_median", "corr_p05", "corr_p95", "corr_skewness"]:
+                    f, ax = plt.subplots(figsize=(14, 4))
+                    a = sns.boxenplot(
+                        y=feature,
+                        x="Stage",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Stage",
+                        box_kws={"alpha": 0.4},
+                        dodge=False,
+                    )
+                    a.legend_.remove()
+                    b = sns.stripplot(
+                        y=feature,
+                        x="Stage",
+                        data=tab_sub,
+                        ax=ax,
+                        hue="Stage",
+                        dodge=False,
+                        zorder=1,
+                        legend=False,
+                    )
+                    sns.despine(ax=ax)
+                    ax.set_title(f"{gen} {name} {feature}")
+                    plt.savefig(
+                        f"{OUT_DIR}/{name}_{gen}_{feature}.pdf", bbox_inches="tight"
+                    )
+                    plt.close(f)
 
 
 def plot(cfg):
