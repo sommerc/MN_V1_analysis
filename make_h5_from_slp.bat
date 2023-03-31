00a6c09e61c7bd905c4b370ecb1bfb2bad7e7438
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-03-31 12:53:27 +0200

    new win h5 conversions

--- make_h5_from_slp.bat
+++ make_h5_from_slp.bat
@@ -1,5 +1,5 @@
 @echo off 
-set list="T:\BIF_StaffSci\Christoph\sweengrp\paper_videos\37-38\FoxP1\144_2022-12-16_14-46-00_FoxP1_Stg3738_12.mp4.predictions.slp" "T:\BIF_StaffSci\Christoph\sweengrp\paper_videos\37-38\FoxP1\144_2022-12-16_15-46-02_FoxP1_Stg3738_22.mp4.predictions.slp" "T:\BIF_StaffSci\Christoph\sweengrp\paper_videos\37-38\En1\143_2022-12-16_13-16-02_En1_Stg3738_22.mp4.predictions.slp" "T:\BIF_StaffSci\Christoph\sweengrp\paper_videos\37-38\En1\143_2022-12-16_12-16-00_En1_Stg3738_12.mp4.predictions.slp"
+set list="Y:\BIF_StaffSci\Christoph\sweengrp\paper_videos\37-38\WT\114_2022-11-11 08-51-17_WT_Stg3538_122.mp4.predictions.slp"
 (for %%f in (%list%) do ( 
    echo sleap-convert %%f --format analysis -o "%%~df%%~pf%%~nf.analysis.h5"
    sleap-convert %%f --format analysis -o "%%~df%%~pf%%~nf.analysis.h5"
