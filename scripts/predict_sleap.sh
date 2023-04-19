9677565a2860c90fd73abef284e3421a8198ac3c
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-04-19 13:37:54 +0200

    updated sbatch script

--- scripts/predict_sleap.sh
+++ scripts/predict_sleap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=slp_37
-#SBATCH --output=logs/%j_%A_%a.out
+#SBATCH --output=logs/%j_%a.out
 
 #SBATCH --array=0-0
 #SBATCH --time=10:00:00
@@ -31,26 +31,38 @@ echo
 
 if [ -z "$1" ]
   then
-    echo "No MOVIE_ROOT_DIR supplied"
+	echo "No MOVIE_ROOT_DIR supplied"
 	exit 1
 fi
 
 if [ -z "$2" ]
   then
-    echo "No centroid model supplied"
+	echo "No centroid model supplied"
 	exit 1
 fi
 
 if [ -z "$3" ]
   then
-    echo "No instance model supplied"
+	echo "No instance model supplied"
 	exit 1
 fi
 
 if [ -z "$4" ]
   then
-    echo "No target instance count (e.g. 1 or 11) specified"
+	echo "No target instance count (e.g. 1 or 11) specified"
 	exit 1
+fi
+
+if [ -z "$5" ]
+  then
+	echo "No overwrite flag set, choose true for overwriting existing files, false for skipping. Default is false"
+	OVERWRITE=false 
+  else
+    if [ "$5" = true ]; then                                                                                                                                                        │
+	  OVERWRITE=true                                                                                                                                                                  │
+	else                                                                                                                                                                            │
+	  OVERWRITE=false                                                                                                                                                                 │
+	fi
 fi
 
 MOVIE_ROOT_DIR=$1
@@ -59,8 +71,8 @@ MODEL_2=$3
 MAX_N_ANIMALS=$4
 
 # enable recursive glob
-shopt -s globstar
-FILES=($MOVIE_ROOT_DIR/**/*.mp4)
+shopt -s globstar nullglob
+FILES=("$MOVIE_ROOT_DIR"/**/*.mp4)
 
 echo  - Found "${#FILES[@]}" movies in $MOVIE_ROOT_DIR
 
@@ -68,15 +80,25 @@ input_mp4="${FILES[${SLURM_ARRAY_TASK_ID}]}"
 output_slp="${input_mp4}.predictions.slp"
 output_h5="${input_mp4}.predictions.analysis.h5"
 
-echo  - Processing array ID ${SLURM_ARRAY_TASK_ID}  
+echo  - Processing array ID ${SLURM_ARRAY_TASK_ID}	
 echo  - File: $input_mp4 
 echo  - Output: $output_slp
 echo  - Centroid Model: $MODEL_1
 echo  - Instance Model: $MODEL_2
 
 
+if [ -f "$output_h5" ] && ["$OVERWRITE" = false]
+  then
+	echo "Output h5 file already present, and overwrite is false skipping"
+	exit 0
+fi
+
+
 #################################################################################
 #################################################################################
+#Example
+# Run on two movies in the 'incoming' folder
+# sbatch --array=0-1 --time=5:00:00 predict_sleap.sh "/nfs/scistore14/sweengrp/zharring/paper_videos/Juv/incoming/" /nfs/scistore14/sweengrp/zharring/paper_videos/Juv/models/230208.centroid.n\=405/ /nfs/scistore14/sweengrp/zharring/paper_videos/Juv/models/230302.centered_instance.n\=410/ 1
 
 srun --cpu_bind=verbose sleap-track "$input_mp4" -m "$MODEL_1" -m "$MODEL_2" -o "$output_slp" --tracking.tracker simple --tracking.match greedy --tracking.similarity centroid --tracking.target_instance_count "$MAX_N_ANIMALS" --tracking.pre_cull_to_target 1  --tracking.post_connect_single_breaks 1 --tracking.clean_instance_count "$MAX_N_ANIMALS" --verbosity rich --batch_size 8 
 
